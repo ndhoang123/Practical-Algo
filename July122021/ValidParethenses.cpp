f4e8112af0d9c56c859d957194e784324252d8f7
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-07-13 10:44:27 +0700

    Optimize solution for ValidParenthenses

--- July122021/ValidParethenses.cpp
+++ July122021/ValidParethenses.cpp
@@ -3,34 +3,21 @@
 using namespace std;
 
 bool isValidParen(string s){
-    int indexMin = 0;
-    int indexMax = s.size() - 1;
-    bool isValid = false;
-    if(s.size() % 2 != 0) return false;
-    for(int i = 0; i < s.size() / 2; i++){
-        if((s[indexMin] == '(' && s[indexMax] == ')') || (s[indexMin] == '[' && s[indexMax] == ']')
-        || (s[indexMin] == '{' && s[indexMax] == '}')){
-            indexMin++;
-            indexMax--;
-            isValid = true;
-            cout << "A" << endl;
-        }
-
-        else if((s[indexMin] == '(' && s[indexMin+1] == ')') || (s[indexMin] == '[' && s[indexMin+1] == ']')
-        || (s[indexMin] == '{' && s[indexMin+1] == '}')){
-            indexMin = indexMin + 2;
-            isValid = true;
-            cout << "B" << endl;
-            cout << indexMin << endl;
+    stack<char> st;
+    for(char c : s){
+        if(c == '(' || c == '{' || c == '['){
+            st.push(c);
         }
         else {
-            cout << "C" << endl;
-            isValid = false;
-            break;
+            if(st.empty()) return false;
+            if(c == ')' && st.top() != '(') return false;
+            if(c == ']' && st.top() != '[') return false;
+            if(c == '}' && st.top() != '{') return false;
+            st.pop();
         }
     }
 
-    return isValid;
+    return st.empty();
 }
 
 int main(){
