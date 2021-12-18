22b74afd325bf28cf863c0c76e6139a0e603fdce
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-18 23:10:37 +0700

    Day 6 - Algo - Permutation In String, explaination

--- Study-Plan-Leetcode/Day6/Algo/PermutationInString.cpp
+++ Study-Plan-Leetcode/Day6/Algo/PermutationInString.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+/*
+    - k is size of s1, and cur, goal array to store value of each of keyword.
+    - We use the window sliding method to find the result, with k.
+    1. Firstly, we iterate all value in s1, and assign it into the cur array. With the index, each of value we count from the 'a' word.
+    2. Secondly, we use loop to iterate all value in s2. 
+    3. We add value in the goal array with the index, couting from the 'a' word. And escalating the value correspond to the index.
+    4. Next, we use condition to check the index is more than the size of s1. If so, they will decrease the value of index in goal array, that index is s2[i - s1.size()] -> the index is gonna point to the position before the k size of current index, the index is in last of k size.
+    5. After that, they will compare between the goal and cur. If satifies, they return true. Otherwise, they must continue.
+*/
+
 bool checkInclusion(string s1, string s2)
 {
     vector<int> cur(26), goal(26);
