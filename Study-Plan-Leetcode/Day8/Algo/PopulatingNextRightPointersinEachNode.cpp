2a963d28d536cd9e726edc2ef598a79cac3df660
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-22 11:38:22 +0700

    Day 8 - Algo - Populate, optimize 1

--- Study-Plan-Leetcode/Day8/Algo/PopulatingNextRightPointersinEachNode.cpp
+++ Study-Plan-Leetcode/Day8/Algo/PopulatingNextRightPointersinEachNode.cpp
@@ -31,56 +31,18 @@ Node *connect(Node *root)
 {
     if (root == NULL)
         return nullptr;
-    queue<Node *> q1, q2;
-    int n = 0;
-    q1.push(root);
-    q2.push(root);
-    while (!q1.empty())
+    queue <Node *> q;
+    q.push(root);
+    while (!q.empty())
     {
-        Node *temp = q1.front();
-        q1.pop();
-        if (temp->left != NULL && temp->right != NULL)
-        {
-            q1.push(temp->left);
-            q2.push(temp->left);
-            q1.push(temp->right);
-            q2.push(temp->right);
+        int n = q.size();
+        for(int i = 0; i < n; i++){
+            Node* temp = q.front();
+            q.pop();
+            if(i!=n-1) temp->next = q.front();
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right); 
         }
-    }
-    while (!q2.empty())
-    {
-        Node *temp;
-        if (n == 0)
-        {
-            temp = q2.front();
-            q2.pop();
-            temp->next = NULL;
-        }
-        else
-        {
-            for (int i = 1; i <= pow(2, n); i++)
-            {
-                if (i == pow(2, n))
-                {
-                    temp->next = q2.front();
-                    temp = q2.front();
-                    temp->next = NULL;
-                    q2.pop();
-                }
-                if (i == 1)
-                {
-                    temp = q2.front();
-                    q2.pop();
-                }
-                if (i > 1 && i < pow(2, n))
-                {
-                    temp->next = q2.front();
-                    temp = q2.front();
-                    q2.pop();
-                }
-            }
-        }
-        n++;
     }
     return root;
 }
