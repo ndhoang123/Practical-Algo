1c6f0b115b271dc3e6406c4da8d9c9249b067404
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-24 21:40:14 +0700

    Day 12 - Algo - Number of 1 Bit, Optimize

--- Study-Plan-Leetcode/Day12/Algo/NumberOf1Bit.cpp
+++ Study-Plan-Leetcode/Day12/Algo/NumberOf1Bit.cpp
@@ -5,12 +5,10 @@ using namespace std;
 
 int hammingWeight(uint32_t n){
     int count = 0;
-    for(int i = 0; i < 32; i++){
-        if((n & 1) == 1){
-            cout << n << " ";
-            ++count;
-        }
-        n = n >> 1;
+    // optimize
+    while(n > 0){
+        ++count;
+        n &= (n-1);
     }
     return count;
 }
