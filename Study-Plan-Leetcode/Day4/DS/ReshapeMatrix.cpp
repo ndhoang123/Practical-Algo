37c10bba449df4fa5b05254a0c1c556bb395fe0b
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-30 21:47:38 +0700

    Day 4 - DS - Reshape Matrix, optimize

--- Study-Plan-Leetcode/Day4/DS/ReshapeMatrix.cpp
+++ Study-Plan-Leetcode/Day4/DS/ReshapeMatrix.cpp
@@ -6,17 +6,22 @@ using namespace std;
 
 vector<vector<int>> matrixReshape(vector<vector<int>> &mat, int r, int c)
 {
-    int rows = mat.size(), cols = mat[0].size(), r1 = 0, c1 = 0;
+    int rows = mat.size(), cols = mat[0].size(), max = rows * cols;
     if(rows*cols != r*c) return mat;
     vector<vector<int>> result(r, vector<int>(c));
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            if(c1 == cols){
-                r1++;
-                c1 = 0;
-            }
-            result[i][j] = mat[r1][c1++];
-        }
+    // My solution, using brute force with O(n^2)
+    // for(int i = 0; i < r; i++){
+    //     for(int j = 0; j < c; j++){
+    //         if(c1 == cols){
+    //             r1++;
+    //             c1 = 0;
+    //         }
+    //         result[i][j] = mat[r1][c1++];
+    //     }
+    // }
+    // The better solution,  O(n)
+    for(int i = 0; i < max; i++){
+        result[i/c][i%c] = mat[i/cols][i%cols];
     }
     return result;
 }
