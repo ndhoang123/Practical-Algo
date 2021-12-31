eff6f55c7e3c0ade01b661440da993649f3957ed
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-31 23:17:09 +0700

    Day 5 - DS - Valid Sudoku, simple brute force

--- Study-Plan-Leetcode/Day5/DS/ValidSudoku.cpp
+++ Study-Plan-Leetcode/Day5/DS/ValidSudoku.cpp
@@ -3,94 +3,25 @@
 
 using namespace std;
 
-bool isExisted(vector<vector<char>> board, int l, int r, int number)
-{
-    unordered_set<int> h;
-    switch (number)
-    {
-    case 1:
-        for (int i = 0; i < r; i++)
-        {
-            if (board[l][i] != '.')
-            {
-                if (h.find(board[l][i]) != h.end())
-                {
-                    return true;
-                }
-                else
-                {
-                    h.insert(board[l][i]);
-                }
-            }
-        }
-        break;
-    case 2:
-        for (int i = 0; i < l; i++)
-        {
-            if (board[i][r] != '.')
-            {
-                if (h.find(board[i][r]) != h.end())
-                {
-                    return true;
-                }
-                else
-                {
-                    h.insert(board[i][r]);
-                }
-            }
-        }
-        break;
-    case 3:
-        for (int i = l; i < l + 3; i++)
-        {
-            for (int j = r; j < r + 3; j++)
-            {
-                if (board[i][j] != '.')
-                {
-                    if (h.find(board[i][j]) != h.end())
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        h.insert(board[i][j]);
-                    }
-                }
-            }
-        }
-        break;
-    }
-    return false;
-}
-
 bool isValidSudoku(vector<vector<char>> &board)
 {
-    for (int i = 0; i < 9; i += 3)
-    {
-        for (int j = 0; j < 9; j += 3)
-        {
-            if (isExisted(board, i, j, 3))
-            {
+    unordered_set<char> row[9];
+    unordered_set<char> cols[9];
+    unordered_set<char> square[9];
+
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            char val = board[i][j];
+            if(val == '.') continue;
+            int pos = (i / 3) * 3 + j / 3;
+            if(row[i].count(val) || cols[j].count(val) || square[pos].count(val)){
                 return false;
             }
+            row[i].insert(val);
+            cols[j].insert(val);
+            square[pos].insert(val);
         }
     }
-    for (int i = 0; i < 9; i++)
-    {
-        if (isExisted(board, i, 9, 1))
-        {
-            return false;
-        }
-    }
-
-    for (int j = 0; j < 9; j++)
-    {
-        if (isExisted(board, 9, j, 2))
-        {
-            return false;
-        }
-    }
-
     return true;
 }
 
