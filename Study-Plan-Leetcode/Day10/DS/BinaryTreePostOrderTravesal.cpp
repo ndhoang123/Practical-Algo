6e4fb02a5f0bcd8649c685d38c5a7f94a389951e
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-05 11:41:12 +0700

    Day 10 - DS - Binary Tree Post Order Travesal, recursive

---
+++ Study-Plan-Leetcode/Day10/DS/BinaryTreePostOrderTravesal.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void treeTraversal(TreeNode *root, vector<int> &result){
+    if(root == NULL){
+        return;
+    }
+    treeTraversal(root->left, result);
+    treeTraversal(root->right, result);
+    result.push_back(root->val);
+}
+
+vector<int> postorderTraversal(TreeNode *root)
+{
+    vector<int> result;
+    treeTraversal(root, result);
+    return result;
+}
+
+int main(){
+    TreeNode* tree = new TreeNode(1);
+    tree->left = new TreeNode(2);
+    tree->right = new TreeNode(3);
+    tree->left->left = new TreeNode(4);
+    tree->left->right = new TreeNode(5);
+    tree->left->left->left = new TreeNode(6);
+    tree->left->left->right = new TreeNode(7);
+    tree->left->right->left = new TreeNode(8);
+    tree->right->left = new TreeNode(9);
+    vector<int> result = postorderTraversal(tree);
+    for(auto it : result){
+        cout << it << " ";
+    }
+    return 0;
+}
