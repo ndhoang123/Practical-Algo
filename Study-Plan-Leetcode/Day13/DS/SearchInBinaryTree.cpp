ea7ec61972d1ed1565f560918b1dfecc31743f4b
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-08 10:01:10 +0700

    Day 13 - DS - Seach BST, using queue

---
+++ Study-Plan-Leetcode/Day13/DS/SearchInBinaryTree.cpp
@@ -0,0 +1,67 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root == NULL) return nullptr;
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        root = q.front();
+        q.pop();
+        if(root->val == val){
+            return root;
+        }
+        if(root->left != NULL){
+            q.push(root->left);
+        }
+        if(root->right != NULL){
+            q.push(root->right);
+        }
+    }
+    return nullptr;
+}
+
+void treeTravesal(TreeNode* tree){
+    queue<TreeNode*> q;
+    q.push(tree);
+    while(!q.empty()){
+        tree = q.front();
+        q.pop();
+        cout << tree->val << " ";
+        if(tree->left != NULL){
+            q.push(tree->left);
+        }
+        if(tree->right != NULL){
+            q.push(tree->right);
+        }
+    }
+}
+
+int main(){
+    TreeNode* tree = new TreeNode(4);
+    tree->left = new TreeNode(2);
+    tree->right = new TreeNode(7);
+    tree->left->left = new TreeNode(1);
+    tree->left->right = new TreeNode(3);
+    tree = searchBST(tree, 5);
+    if(tree == nullptr){
+        cout << 0;
+    }
+    else{
+        treeTravesal(tree);
+    }
+    return 0;
+}
