c573cbc61029551db819eb99c754b9de09c0bc86
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-08 10:04:43 +0700

    Day 13 - DS - Seach BST, using recursive

--- Study-Plan-Leetcode/Day13/DS/SearchInBinaryTree.cpp
+++ Study-Plan-Leetcode/Day13/DS/SearchInBinaryTree.cpp
@@ -16,22 +16,13 @@ struct TreeNode
 
 TreeNode* searchBST(TreeNode* root, int val) {
     if(root == NULL) return nullptr;
-    queue<TreeNode*> q;
-    q.push(root);
-    while(!q.empty()){
-        root = q.front();
-        q.pop();
-        if(root->val == val){
-            return root;
-        }
-        if(root->left != NULL){
-            q.push(root->left);
-        }
-        if(root->right != NULL){
-            q.push(root->right);
-        }
+    if(root->val == val) return root;
+    if(root->val < val){
+        return searchBST(root->left, val);
     }
-    return nullptr;
+    else{
+        return searchBST(root->right, val);
+    }
 }
 
 void treeTravesal(TreeNode* tree){
