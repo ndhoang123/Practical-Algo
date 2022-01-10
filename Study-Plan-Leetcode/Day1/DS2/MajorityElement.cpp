6d48260485e35afc812b3c0b9a319ae5b76e316e
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-10 10:16:34 +0700

    Day 1 - DS2 - Majority Element, hash optimize

--- Study-Plan-Leetcode/Day1/DS2/MajorityElement.cpp
+++ Study-Plan-Leetcode/Day1/DS2/MajorityElement.cpp
@@ -6,29 +6,17 @@ using namespace std;
 
 int majorityElement(vector<int> &nums)
 {
-    if(nums.empty()) return 0;
     unordered_map<int, int> h;
-    int max = 0, temp = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if(h.find(nums[i]) != h.end()){
-            h[nums[i]]++;
-        }
-        else{
-            h[nums[i]] = 1;
+    for(auto num : nums){
+        if(++h[num] > nums.size() / 2){
+            return num;
         }
     }
-    for(auto it : h){
-        if(it.second > temp){
-            temp = it.second;
-            max = it.first;
-        }
-    }
-    return max;
+    return 0;
 }
 
 int main(){
-    vector<int> nums{2,2,1,1,1,2,2};
+    vector<int> nums{3,2,3};
     int result = majorityElement(nums);
     cout << result;
     return 0;
