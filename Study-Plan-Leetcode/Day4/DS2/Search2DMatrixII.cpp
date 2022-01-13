8ae229003e4fc63a5195142a4d8469ed4b8d8dc7
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-13 10:14:41 +0700

    Day 4 - DS2 - Search 2D Matrix II, O(n + m)

--- Study-Plan-Leetcode/Day4/DS2/Search2DMatrixII.cpp
+++ Study-Plan-Leetcode/Day4/DS2/Search2DMatrixII.cpp
@@ -7,30 +7,26 @@ using namespace std;
 bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
     if(matrix.empty()) return false;
-    int left, right;
     int row = matrix.size(), col = matrix[0].size();
-    for(int i = 0; i < row; i++){
-        left = 0;
-        right = col-1;
-        while(left <= right){
-            int mid = (left + right)/2;
-            if(matrix[i][mid] == target){
-                return true;
-            }
-            if(matrix[i][mid] < target){
-                left = mid + 1;
-            }
-            else if(matrix[i][mid] > target){
-                right = mid - 1;
-            }
+    int left = 0, right = col - 1;
+    while(left < row && right >= 0){
+        if(matrix[left][right] == target){
+            return true;
+        }
+        if(matrix[left][right] > target){
+            right--;
+        }
+        else{
+            left++;
         }
     }
     return false;
 }
 
 int main(){
-    vector<vector<int>> matrix{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, 
-    {10,13,14,17,24}, {18,21,23,26,30}};
+    // vector<vector<int>> matrix{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, 
+    // {10,13,14,17,24}, {18,21,23,26,30}};
+    vector<vector<int>> matrix{{-5}};
     int target;
     cin >> target;
     searchMatrix(matrix, target) ? cout << "True" : cout << "False";
