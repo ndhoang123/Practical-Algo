808dd3b0154571a62541ac97e1e1e3ef00ff3d29
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-27 11:37:30 +0700

    Day 18 - DS2 - Serialize and Deserialize Binary Stree, using isstream and ostreadm

---
+++ Study-Plan-Leetcode/Day18/DS2/SerializeandDeserializeBinaryTree.cpp
@@ -0,0 +1,84 @@
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Codec {
+private:
+    void serialize(TreeNode* root, ostringstream& out) {
+        if (root) {
+            out << root->val << ' ';
+            serialize(root->left, out);
+            serialize(root->right, out);
+        } else {
+            out << "# ";
+        }
+    }
+
+    TreeNode* deserialize(istringstream& in) {
+        string val;
+        in >> val;
+        if (val == "#")
+            return nullptr;
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserialize(in);
+    }
+};
+
+void treeTraversal(TreeNode *result)
+{
+    queue<TreeNode *> q;
+    q.push(result);
+    while (!q.empty())
+    {
+        TreeNode *temp = q.front();
+        q.pop();
+        cout << temp->val << " ";
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(5);
+    Codec ser, deser;
+    cout << ser.serialize(root);
+    TreeNode* ans = deser.deserialize(ser.serialize(root));
+    treeTraversal(ans);
+    return 0;
+}
