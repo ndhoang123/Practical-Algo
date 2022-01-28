6e7c98cc940c5649b32fa37c34531566b1c90fad
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-01-28 22:16:26 +0700

    Day 19 - DS2 - Minimum Number of Vertices to Reach All Nodes, shorter version

--- Study-Plan-Leetcode/Day19/DS2/MinimumNumberofVerticestoReachAllNodes.cpp
+++ Study-Plan-Leetcode/Day19/DS2/MinimumNumberofVerticestoReachAllNodes.cpp
@@ -6,31 +6,24 @@ using namespace std;
 
 vector<int> findSmallestSetOfVertices(int n, vector<vector<int>> &edges)
 {
-    unordered_set<int> head;
-    unordered_set<int> tail;
-    vector<int> result;
-    for (int i = 0; i < edges.size(); i++)
-    {
-        if(head.find(edges[i][0]) == head.end() && tail.find(edges[i][0]) == tail.end()){
-            head.insert(edges[i][0]);
-        }
-        if(tail.find(edges[i][1]) == tail.end()){
-            if(head.find(edges[i][1]) != head.end()){
-                head.erase(edges[i][1]);
-            }
-            tail.insert(edges[i][1]);
-        }
+    // The non in-degree element will be returned. Because non in-degree will not from the other. Otherwise,
+    // the in-degree may from the other element. Hence, all non in-degree will be returned in our solution.
+    vector<int> result, seen(n);
+    for(auto it : edges){
+        seen[it[1]] = 1;
     }
-    for (auto it : head)
+    for (int i = 0; i < n; i++)
     {
-        result.push_back(it);
+        if(seen[i] == 0){
+            result.push_back(i);
+        }
     }
     return result;
 }
 
 int main(){
     vector<vector<int>> edges{{0,1}, {2,1}, {3,1}, {1,4}, {2,4}};
-    vector<int> result = findSmallestSetOfVertices(6, edges);
+    vector<int> result = findSmallestSetOfVertices(5, edges);
     for (auto it : result)
     {
         cout << it << " ";
