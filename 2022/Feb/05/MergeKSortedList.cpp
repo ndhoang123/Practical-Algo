0289922f950534427c35f08f69295d46e3f63996
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-05 22:06:04 +0700

    Feb challenge - Merge K Sorted List, divine and conquer

--- 2022/Feb/05/MergeKSortedList.cpp
+++ 2022/Feb/05/MergeKSortedList.cpp
@@ -13,45 +13,56 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+    if(l1 == nullptr){
+        return l2;
+    }
+    if(l2 == nullptr){
+        return l1;
+    }
+    if(l1->val <= l2->val){
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    }
+    else{
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+
 ListNode *mergeKLists(vector<ListNode *> &lists)
 {
-    if(lists.empty()) return nullptr;
-    priority_queue<pair<int, ListNode*>> he;
-    for(int i = 0; i < lists.size(); i++){
-        ListNode* temp = lists[i];
-        while(temp != nullptr){
-            he.push({temp->val, temp});
-            temp = temp->next;
-        }
+    if(lists.empty()){
+        return nullptr;
     }
-    ListNode* result = nullptr;
-    while(!he.empty()){
-        ListNode* temp = he.top().second;
-        he.pop();
-        temp->next = result;
-        result = temp;
+
+    while(lists.size() > 1){
+        lists.push_back(mergeTwoLists(lists[0], lists[1]));
+        lists.erase(lists.begin());
+        lists.erase(lists.begin());
     }
-    return result;
+
+    return lists.front();
 }
 
 int main(){
     vector<ListNode*> lists{{}};
-    // ListNode* n1 = new ListNode(1);
-    // n1->next = new ListNode(4);
-    // n1->next->next = new ListNode(5);
-    // ListNode* n2 = new ListNode(1);
-    // n2->next = new ListNode(3);
-    // n2->next->next = new ListNode(4);
-    // ListNode* n3 = new ListNode(2);
-    // n3->next = new ListNode(6);
-    // lists.push_back(n1);
-    // lists.push_back(n2);
-    // lists.push_back(n3);
+    ListNode* n1 = new ListNode(1);
+    n1->next = new ListNode(4);
+    n1->next->next = new ListNode(5);
+    ListNode* n2 = new ListNode(1);
+    n2->next = new ListNode(3);
+    n2->next->next = new ListNode(4);
+    ListNode* n3 = new ListNode(2);
+    n3->next = new ListNode(6);
+    lists.push_back(n1);
+    lists.push_back(n2);
+    lists.push_back(n3);
     ListNode* result = mergeKLists(lists);
-    // while(result != nullptr){
-    //     cout << result->val << " ";
-    //     result = result->next;
-    // }
-    result == nullptr ? cout << "null" : cout << "Not null";
+    while(result != nullptr){
+        cout << result->val << " ";
+        result = result->next;
+    }
+    // result == nullptr ? cout << "null" : cout << "Not null";
     return 0;
 }
