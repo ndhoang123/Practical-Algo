87cf7b0806c5b0b968eb7fc69c412e6b93c88135
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-06 22:43:20 +0700

    DP Pratical - Climbing stairs, using DP with SC: O(1)

--- Theory/DP/DP/ClimbingStairs.cpp
+++ Theory/DP/DP/ClimbingStairs.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 int climbStairs(int n) {
     if(n == 1) return 1;
-    vector<int> dp(n+1);
-    dp[1] = 1;
-    dp[2] = 2;
+    if(n == 2) return 2;
+    int lastestStair = 2, lastStair = 1, currentStair;
     for(int i = 3; i <= n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+        currentStair = lastestStair + lastStair;
+        lastStair = lastestStair;
+        lastestStair = currentStair;
     }
-    return dp[n];
+    return currentStair;
 }
 
 int main(){
