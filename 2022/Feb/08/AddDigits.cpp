be2acac7df268fa0b994290282fd3f23a496cf7a
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-08 20:41:39 +0700

    Feb challenge - Add Digits, using math (Digital Root)

--- 2022/Feb/08/AddDigits.cpp
+++ 2022/Feb/08/AddDigits.cpp
@@ -5,16 +5,9 @@
 using namespace std;
 
 int addDigits(int num){
-    int result = 0;
-    while(num > 0){
-        result += num%10;
-        num = num/10;
-        if(num == 0 && result > 9){
-            num = result;
-            result = 0;
-        }
-    }
-    return result;
+    if(num == 0) return 0;
+    if(num %9 == 0) return 9;
+    return num % 9;
 }
 
 int main(){
