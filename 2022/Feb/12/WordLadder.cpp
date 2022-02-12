e98e69d42b28322a6f522094779251dd34d7ad78
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-12 23:10:22 +0700

    Feb challenge - Word Ladder, using BFS

---
+++ 2022/Feb/12/WordLadder.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/word-ladder/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+{
+    unordered_set<string> dict(wordList.begin(), wordList.end());
+    queue<string> todo;
+    todo.push(beginWord);
+    int ladder = 1;
+    while (!todo.empty())
+    {
+        int n = todo.size();
+        for(int i = 0; i < n; i++){
+            string word = todo.front();
+            todo.pop();
+            if(word == endWord){
+                return ladder;
+            }
+            dict.erase(word);
+            for(int j = 0; j < word.size(); j++){
+                char c = word[j];
+                for(int k = 0; k < 26; k++){
+                    word[j] = 'a' + k;
+                    if(dict.find(word) != dict.end()){
+                        todo.push(word);
+                    }
+                }
+                word[j] = c;
+            }
+        }
+        ladder++;
+    }
+    return 0;
+}
+
+int main()
+{
+    string beginWord = "hit", endWord = "cog";
+    vector<string> wordList{"hot","dot","dog","lot","log","cog"};
+    int result = ladderLength(beginWord, endWord, wordList);
+    cout << result;
+    return 0;
+}
