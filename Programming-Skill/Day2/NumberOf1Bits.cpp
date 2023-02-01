3c44e38804dc9120e38473b9bfb84bb5937b139c
Hoang Nguyen Dang <hoangnguyen12397@gmail.com>
2023-02-01 14:17:28 +0700

    [Programming - Day 2] Number of 1 Bits - Better approach

--- Programming-Skill/Day2/NumberOf1Bits.cpp
+++ Programming-Skill/Day2/NumberOf1Bits.cpp
@@ -8,8 +8,8 @@ int hammingWeight(uint32_t n)
 {
     int count = 0;
     while (n > 0){
-        count += n & 1;
-        n = n >> 1;
+        n &= (n - 1);
+        count++;
     }
     return count;
 }
