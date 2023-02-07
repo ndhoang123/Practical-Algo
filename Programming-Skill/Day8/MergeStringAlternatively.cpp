7756178634ca21a5081930563cac4c3d625f2f70
Hoang Nguyen Dang <hoangnguyen12397@gmail.com>
2023-02-07 14:23:15 +0700

    [Programming skill - Day 8] Merge string alternatively

---
+++ Programming-Skill/Day8/MergeStringAlternatively.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/find-the-difference/?envType=study-plan&id=programming-skills-i
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string mergeAlternately(string word1, string word2)
+{
+    string sc = "";
+    int i = 0;
+    while(i < word1.size() || i < word2.size())
+    {
+        if(i < word1.size()){
+            sc += word1[i];
+        }
+        if(i < word2.size()){
+            sc += word2[i];
+        }
+        i++;
+    }
+    return sc;
+}
+
+int main()
+{
+    string word1 = "abc";
+    string word2 = "pqr";
+    string rs = mergeAlternately(word1, word2);
+    cout << rs;
+    return 0;
+}
