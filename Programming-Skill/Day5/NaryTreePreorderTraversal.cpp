92d56f98cfc2790893299af4d2b031d7c5a167ab
Hoang Nguyen Dang <hoangnguyen12397@gmail.com>
2023-02-11 22:33:38 +0700

    [Programming skill - Day 5] N-ary Tree Preorder Traversal - using stack

--- Programming-Skill/Day5/NaryTreePreorderTraversal.cpp
+++ Programming-Skill/Day5/NaryTreePreorderTraversal.cpp
@@ -3,53 +3,60 @@
 
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int val;
-    vector<Node*> children;
+    vector<Node *> children;
 
     Node() {}
 
-    Node(int _val) {
+    Node(int _val)
+    {
         val = _val;
     }
 
-    Node(int _val, vector<Node*> _children) {
+    Node(int _val, vector<Node *> _children)
+    {
         val = _val;
         children = _children;
     }
 };
 
-void travel(Node *root, vector<int> &vt)
-{
-    if (root == nullptr)
-    {
-        return;
-    }
-    vt.push_back(root->val);
-    for (Node *child : root->children)
-    {
-        travel(child, vt);
-    }
-}
-
 vector<int> preorder(Node *root)
 {
     vector<int> result;
-    travel(root, result);
+    if (root == nullptr)
+        return result;
+
+    stack<Node *> stk;
+    stk.push(root);
+    while (!stk.empty())
+    {
+        Node *cur = stk.top();
+        stk.pop();
+        result.push_back(cur->val);
+        for (int i = cur->children.size() - 1; i >= 0; i--)
+        {
+            if (cur->children[i] != nullptr)
+            {
+                stk.push(cur->children[i]);
+            }
+        }
+    }
     return result;
 }
 
 int main()
 {
-    Node* root = new Node(1);
+    Node *root = new Node(1);
     root->children.push_back(new Node(2));
     root->children.push_back(new Node(3));
     root->children.push_back(new Node(4));
     root->children[0]->children.push_back(new Node(5));
     root->children[0]->children.push_back(new Node(6));
     vector<int> rs = preorder(root);
-    for(int i = 0; i < rs.size(); i++)
+    for (int i = 0; i < rs.size(); i++)
     {
         cout << rs[i] << " ";
     }
